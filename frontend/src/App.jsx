e35a654816b5a33850c40a575c19421da342d56f
Dhameliyachintan <chintandhameliya07@gmail.com>
2024-10-01 08:37:42 +0530

    login and SignUpPage

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+// import LoginPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signuppage" element={<SignUpPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
